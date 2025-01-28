098cd6e0fa44a352bd3da52bed8ad87e56e30a78
Aminbaxxx <aminbaxxxx@gmail.com>
2025-01-28 12:34:54 +0330

    Create New Folder structure

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+import { getAllCities } from "../../utils/shared.js";
+
+window.addEventListener("load", () => {
+  const loadingContainer = document.querySelector('#loading-container')
+
+  getAllCities().then((response) => {
+    loadingContainer.style.display = 'none'
+    const popularCitiesContainer = document.querySelector("#popular-cities");
+    const popularCities = response.data.cities.filter((city) => city.popular);
+
+    popularCities.forEach((city) => {
+      popularCitiesContainer.insertAdjacentHTML(
+        "beforeend",
+    `
+        <li class="main__cities-item">
+            <p class="main__cities-link">${city.name}</p>
+        </li>
+      `
+      );
+    });
+  });
+});
